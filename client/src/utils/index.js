c52c398e3684f929101f0e5acb15612d07b5130a
AirborneCoding <airborneCoding@gmail.com>
2024-01-28 20:02:31 +0100

    edit

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,6 @@
 // export const developmentUrlGraphql = "http://localhost:5000/graphql"
-export const developmentUrlGraphql = "https://graphql-test-omega.vercel.app/graphql"
-
+// export const developmentUrlGraphql = "https://graphqltest-jyfm.onrender.com/graphql"
+export const developmentUrlGraphql = "https://graphql-test-o19qdt9j3-airbornecoding.vercel.app/graphql"
 
 // https://graphql-test-omega.vercel.app/
+// https://graphql-test-o19qdt9j3-airbornecoding.vercel.app/
