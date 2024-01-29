a7b875863481c8954c10e5e2fe4d51b352c4dd2a
AirborneCoding <airborneCoding@gmail.com>
2024-01-29 12:14:22 +0100

    edit

--- client/src/content/books/components/BooksData.jsx
+++ client/src/content/books/components/BooksData.jsx
@@ -1,34 +1,36 @@
-import React, { Suspense } from "react";
+import React, { lazy,Suspense } from "react";
 import { useFetchBooks } from "../hooks/useBooksHooks"
-import BookItems from "./BookItems"
+// import BookItems from "./BookItems"
+
+const BookItems = lazy(() => import('./BookItems'));
 
 const BooksData = () => {
 
     const { booksData, isLoading, isError, error } = useFetchBooks()
 
-    if (isLoading) {
-        return <div>Loading...</div>
-    }
+    // if (isLoading) {
+    //     return <div>Loading...</div>
+    // }
 
     if (isError) {
         console.log("hola", error);
         return <div>something went wrong...</div>
     }
 
-    // console.log(booksData);
-
     return (
         <section className="my-12">
             <h1 className="text-2xl ">
                 Books , on stock :
             </h1><hr className="mb-5" />
-            <div className="grid grid-cols-2 gap-11">
-                {
-                    booksData?.map((p) => {
-                        return <BookItems key={p?._id} {...p} />
-                    })
-                }
-            </div>
+            <Suspense fallback={<div>Loading2...</div>}>
+                <div className="grid grid-cols-2 gap-11">
+                    {
+                        booksData?.map((p) => {
+                            return <BookItems key={p?._id} {...p} />
+                        })
+                    }
+                </div>
+            </Suspense>
         </section>
     )
 
